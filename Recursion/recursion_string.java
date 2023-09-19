bae0833bfe94f39840d4f1b45ec6d4f814227cdb
Adarsh Kunal <78250787+kunaladarsh@users.noreply.github.com>
2023-09-19 18:19:55 +0530

    Update recursion_string.java

--- Recursion/recursion_string.java
+++ Recursion/recursion_string.java
@@ -1,1 +1,59 @@
 
+// Word Search
+
+public static Node segregateEvenOdd(Node head)
+
+    {
+        ArrayList<Integer>arr = new ArrayList<>();
+        Node temp=head;
+
+        while(temp!=null)
+
+        {
+
+            arr.add(temp.data);
+
+            temp=temp.next;
+
+        }
+
+        temp=head;
+
+        for(int a : arr)
+
+        {
+
+            if(a%2==0)
+
+            {
+
+                temp.data=a;
+
+                temp=temp.next;
+
+            }
+
+        }
+
+        for(int a : arr)
+
+        {
+
+            if(a%2!=0)
+
+            {
+
+                temp.data=a;
+
+                temp=temp.next;
+
+            }
+
+        }
+
+        return head;
+
+    }
+
+}
+
